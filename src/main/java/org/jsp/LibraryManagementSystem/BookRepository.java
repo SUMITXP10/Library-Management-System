13053af3c72095c5434a6f60b8d8e74aca2310ac
Sumit Ranjan Satapathy <satapathysumit10@gmail.com>
2025-08-17 13:12:17 +0530

    Initial commit - Library Management System

---
+++ src/main/java/org/jsp/LibraryManagementSystem/BookRepository.java
@@ -0,0 +1,7 @@
+package org.jsp.LibraryManagementSystem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
---
+++ src/test/java/org/jsp/LibraryManagementSystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.LibraryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
